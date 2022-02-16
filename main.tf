54deceb6229efe385fcac44a5a8ced3c97dcad1e
vijayyesh <95198082+vijayyesh@users.noreply.github.com>
2022-02-16 10:18:49 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+  required_version = ">= 0.14.9"
+}
+provider "aws" {
+  profile = "default"
+  region  = "us-east-1"
+}
+resource "aws_instance" "app_server" {
+  ami           = "ami-09ac68f361e5f4a13"
+  instance_type = "t2.small"
+  tags = {
+    Name = "ExampleAppServerInstance"
+  }
+}
